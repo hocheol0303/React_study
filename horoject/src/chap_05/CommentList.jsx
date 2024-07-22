cfafb2bdbc29b94dd7d76e09b2ab6eaea34727c8
양호철 <hocheol0303@gmail.com>
2024-07-22 21:46:29 +0900

    chap_05 map 함수를 활용하여 배열 파라미터 처리하기

--- horoject/src/chap_05/CommentList.jsx
+++ horoject/src/chap_05/CommentList.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import Comment from './Comment'
 
+const comments =[
+    {
+        name:'이인제',
+        comment:'안녕하세요 소플입니다.',
+    },
+    {
+        name:'호빵맨' ,
+        comment:'세균맨 혼내주는 우리 호빵맨' ,
+    },
+    {
+        name:'강강수월래',
+        comment:'민속놀이'
+    },
+];
+
 function CommentList(props){
     return(
         <div>
             <Comment name={'이인제'} comment={'My first component'}/>
             <Comment name={'호빵맨'} comment={'밀가루로 만든 호빵맨'}/>
             <Comment name={'컴포넌트란'} comment={'붕어빵틀 하나 만들어놓고 재료만 바꾸면 와다다다다다'}/>
+
+            {/* 여러개 담아놓고 map함수를 사용하여 모조리 컴포넌트로 출력하자 */}
+            {comments.map((comment) => {
+                return (
+                    <Comment name={comment.name} comment={comment.comment}/>
+                );
+            })}
         </div>
     )
 }
