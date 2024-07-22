808c0128d8d80208e8f5c0045f5ea328c81ca9c6
양호철 <hocheol0303@gmail.com>
2024-07-22 16:00:24 +0900

    React_chap04

--- horoject/src/chap_03/Library.jsx
+++ horoject/src/chap_03/Library.jsx
@@ -4,6 +4,7 @@ import Book from "./Book";
 function Library(props) {
     return (
         <div>
+            {/* Book 함수에 속성을 지명해서 파라미터를 넘겨주는 느낌이다 */}
             <Book name="처음 만난 파이썬" numOfPage={300} />
             <Book name="처음 만난 AWS" numOfPage={400} />
             <Book name="처음 만난 리액트" numOfPage={500} />
--- horoject/src/index.js
+++ horoject/src/index.js
@@ -4,13 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Library from './chap_03/Library';
+import Clock from './chap_04/Clock';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Library />
-  </React.StrictMode>
-);
+
+// chap_04: setInterval 함수로 업데이트 하기
+setInterval(() => {
+  root.render(
+      // chap_03: 리액트로 웹페이지 생성하기
+    <React.StrictMode>
+      <Library />
+      <Clock />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
